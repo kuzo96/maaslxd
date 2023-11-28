4974e808cbce14aa8578f51315677c952c4df012
unknown <Ha Local-Win>
2023-11-28 14:42:02 +0700

    8/10 commit 6

--- my-libre/docker-php-entrypoint.sh
+++ my-libre/docker-php-entrypoint.sh
@@ -9,12 +9,12 @@ set -e
 #exec "$@"
 tar -xf /librenms.tar.gz -C /opt &
 tar -xf /mariadb.tar.gz -C /opt &
-/usr/sbin/php-fpm8.1 &
-/usr/sbin/nginx &
-/usr/bin/mysqld_safe
-/usr/sbin/syslog-ng &
-/usr/sbin/cron -f /etc/cron.d/librenms &
+php-fpm8.1 &
+nginx &
+mysqld_safe
+syslog-ng &
+cron -f /etc/cron.d/librenms &
 cd /opt/librenms &
-/usr/bin/env php artisan schedule:run --no-ansi --no-interaction &
+env php artisan schedule:run --no-ansi --no-interaction &
 tail -f /dev/null
 
